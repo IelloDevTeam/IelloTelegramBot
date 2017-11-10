6498b0bb505e8a1ee07db827268ee2eeae85a7b0
AlessiaVe <aleventa97@gmail.com>
2017-11-10 11:24:04 +0100

    modificati messaggi di risposta

--- model/RadiusCommand.class.php
+++ model/RadiusCommand.class.php
@@ -14,10 +14,15 @@ require_once ("TelegramChainElement.class.php");
  */
 class RadiusCommand extends TelegramChainElement
 {
+    /* costanti per le risposte */
+    const ERROR = \xF0\x9F\x9A\xAB;
+    const MISUNDERSTANDING = \xF0\x9F\x98\xB3;
+    const READY = \xE2\x9C\x94;
+    const CAR = \xF0\x9F\x9A\x97;
     /** Risposte **/
-    const RADIUS_SETTED = "Ok, quando vorrai richercherò parcheggi ad una distanza massima di: ";
-    const RADIUS_ZERO = "Il raggio non può essere uguale a zero!";
-    const RADIUS_INVALID = "Non ho capito!\nUso del comando: /raggio <distanza_in_metri>";
+    const RADIUS_SETTED = "Ok". RadiusCommand::READY.", quando vorrai richercherò parcheggi".RadiusCommand::CAR." ad una distanza massima di: ";
+    const RADIUS_ZERO = "Il raggio non può essere uguale a zero!".RadiusCommand::ERROR;
+    const RADIUS_INVALID = "Non ho capito!".RadiusCommand::MISUNDERSTANDING."\nUso del comando: /raggio <distanza_in_metri>";
 
     protected function onMessage($chatId, $userId, $value, $next)
     {
